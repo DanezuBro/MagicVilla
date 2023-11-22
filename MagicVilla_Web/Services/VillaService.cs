87835fbdf90e7a202732113b5359cd2fc8ee1ad2
scortarudaniel <scortarudaniel@yahoo.com>
2023-11-22 19:54:16 +0200

    Add BAseService and VillaService to Web MVC

---
+++ MagicVilla_Web/Services/VillaService.cs
@@ -0,0 +1,64 @@
+﻿using MagicVilla_Utility;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+
+namespace MagicVilla_Web.Services
+{
+    public class VillaService : BaseService, IVillaService
+    {
+        private IHttpClientFactory _clientFactory { get; set; }
+        private string vilaUrl;
+        public VillaService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+            vilaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI")!;
+        }
+        public Task<T> CreateAsync<T>(VillaCreateDTO dto)
+        {
+            return SendAsync<T>(new APIRequest 
+            {
+                ApiType = SD.ApiType.POST,
+                Data = dto,
+                Url = vilaUrl + "/api/VillaAPI"
+            });
+        }
+
+        public Task<T> DeleteAsync<T>(int id)
+        {
+            return SendAsync<T>(new APIRequest
+            {
+                ApiType = SD.ApiType.DELETE,
+                Url = vilaUrl + "/api/VillaAPI/" + id
+            });
+        }
+
+        public Task<T> GetAllAsync<T>()
+        {
+            return SendAsync<T>(new APIRequest
+            {
+                ApiType = SD.ApiType.GET,
+                Url = vilaUrl + "/api/VillaAPI"
+            });
+        }
+
+        public Task<T> GetAsync<T>(int id)
+        {
+            return SendAsync<T>(new APIRequest
+            {
+                ApiType = SD.ApiType.GET,
+                Url = vilaUrl + "/api/VillaAPI/" + id
+            });
+        }
+
+        public Task<T> UpdateAsync<T>(VillaUpdateDTO dto)
+        {
+            return SendAsync<T>(new APIRequest
+            {
+                ApiType = SD.ApiType.PUT,
+                Data = dto,
+                Url = vilaUrl + "/api/VillaAPI/" + dto.Id
+            });
+        }
+    }
+}
